30c91ba4a19a07be1154925361c734f66be24725
JULIO DAVID CARDONA MELENDEZ <163765285+chronischer-zauderer@users.noreply.github.com>
2025-05-03 20:08:51 +0000

    cambios

--- app/src/main/scala/taller/Operaciones.scala
+++ app/src/main/scala/taller/Operaciones.scala
@@ -2,51 +2,60 @@ package taller
 
 import taller.Tipos._
 import scala.annotation.tailrec
-class Operaciones {
 
+class Operaciones {
 
   def esUnico(estado: Estado): Boolean = {
     val listaTotal = estado._1 ++ estado._2 ++ estado._3
     listaTotal.distinct.length == listaTotal.length
   }
 
-  def aplicarMovimiento(e: Estado, m: Movimiento): Estado = m match {
+  def estadoValido(estado: Estado): Boolean = esUnico(estado)
 
+  val posiblesMovimientos: List[Movimiento] = ((for {
+    n <- -10 to 10 if n != 0
+  } yield Uno(n)) ++ (for {
+    n <- -10 to 10 if n != 0
+  } yield Dos(n))).toList
+
+  def aplicarMovimiento(e: Estado, m: Movimiento): Estado = m match {
     case Uno(0) => e
 
     case Uno(n) if n > 0 =>
       val (rest, mov) = e._1.splitAt(e._1.length - n)
       val nuevoEstado = (rest, mov ++ e._2, e._3)
-      if (esUnico(nuevoEstado)) nuevoEstado else throw new IllegalArgumentException("Los vagones no deben contener elementos duplicados.")
+      if (esUnico(nuevoEstado)) nuevoEstado else e
 
     case Uno(n) if n < 0 =>
       val (mov, unorest) = e._2.splitAt(math.abs(n))
       val nuevoEstado = (e._1 ++ mov, unorest, e._3)
-      if (esUnico(nuevoEstado)) nuevoEstado else throw new IllegalArgumentException("Los vagones no deben contener elementos duplicados.")
+      if (esUnico(nuevoEstado)) nuevoEstado else e
 
     case Dos(0) => e
 
     case Dos(n) if n > 0 =>
       val (rest, mov) = e._1.splitAt(e._1.length - n)
-      val nuevoEstado = (rest, e._2, mov ++ e._3  )
-      if (esUnico(nuevoEstado)) nuevoEstado else throw new IllegalArgumentException("Los vagones no deben contener elementos duplicados.")
+      val nuevoEstado = (rest, e._2, mov ++ e._3)
+      if (esUnico(nuevoEstado)) nuevoEstado else e
 
     case Dos(n) if n < 0 =>
       val (mov, dosrest) = e._3.splitAt(math.abs(n))
       val nuevoEstado = (e._1 ++ mov, e._2, dosrest)
-      if (esUnico(nuevoEstado)) nuevoEstado else throw new IllegalArgumentException("Los vagones no deben contener elementos duplicados.")
+      if (esUnico(nuevoEstado)) nuevoEstado else e
 
     case _ => e
   }
 
-  def aplicarMovimientos(e:Estado , movs:Maniobra): List [Estado] = {
+  def aplicarMovimientos(e: Estado, movs: Maniobra): List[Estado] = {
     @tailrec
-    def aplicarMovimientosAux(movs:Maniobra , acc:List [Estado]):List [Estado] = movs match {
+    def aplicarMovimientosAux(movs: Maniobra, acc: List[Estado]): List[Estado] = movs match {
       case Nil => acc.reverse
-      case m::ms =>
+      case m :: ms =>
         val nuevoEstado = aplicarMovimiento(acc.head, m)
         aplicarMovimientosAux(ms, nuevoEstado :: acc)
-      }
-      aplicarMovimientosAux(movs, List (e))
-  }  
+    }
+    aplicarMovimientosAux(movs, List(e))
+  }
+
+  
 }
